eba4da5081fb784f074f537e75ec537507f4e40d
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-02-02 00:04:06 -0300

    projeto criado

---
+++ src/main/java/com/guilhermesantos/projecthexagonal/ProjecthexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermesantos.projecthexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecthexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecthexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermesantos/projecthexagonal/ProjecthexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermesantos.projecthexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjecthexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
