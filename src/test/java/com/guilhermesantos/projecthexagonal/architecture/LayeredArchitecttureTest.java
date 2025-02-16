4b1e511f825679f5a4caf4c7763f19f5cdc84a8c
Guilherme dos Santos <guilherme1996santos@icloud.com>
2025-02-16 17:05:33 -0300

    definindo o acesso entre camadas

--- src/test/java/com/guilhermesantos/projecthexagonal/architecture/LayeredArchitecttureTest.java
+++ src/test/java/com/guilhermesantos/projecthexagonal/architecture/LayeredArchitecttureTest.java
@@ -1,4 +1,23 @@
 package com.guilhermesantos.projecthexagonal.architecture;
 
+@AnalyzeClassees(packages = "com.guilhermesantos.projecthexagonal")
+@SpringBootTest
 public class LayeredArchitecttureTest {
+
+
+    @ArchTest
+    public static final ArchRule layered_architecture_test = layreedArchitecture()
+            .consideringAllDependencies()
+            .layer("AdaptersIn").definedBy("..adapters.in..")
+            .layer("AdaptersOut").definedBy("..adapters.out..")
+            .layer("UseCase").definedBy("..application.core.usecase..")
+            .layer("PortsIn").definedBy("..application.ports.in..")
+            .layer("PortsOut").definedBy("..application.ports.out..")
+            .layer("Config").definedBy("..config..")
+            .whereLayer("AdaptersIn").mayOnlyBeAcessedByLayers("Config")
+            .whereLayer("AdaptersOut").mayOnlyBeAcessedByLayers("Config")
+            .whereLayer("UseCase").mayOnlyBeAcessedByLayers("Config")
+            .whereLayer("PortsIn").mayOnlyBeAcessedByLayers("UseCase", "AdaptersIn")
+            .whereLayer("PortsOut").mayOnlyBeAcessedByLayers("UseCase", "AdaptersOut")
+            .whereLayer("Config").mayNotBeAccessedByAnyLayer();
 }
